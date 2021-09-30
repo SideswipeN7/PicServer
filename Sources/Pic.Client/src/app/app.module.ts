c9c5405a5bc9a859d9a47fa176aab9bdecba32fc
Paweł Wanot <pawel.wanot@codefit.pl>
2021-09-30 14:05:37 +0200

    update

--- Sources/Pic.Client/src/app/app.module.ts
+++ Sources/Pic.Client/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { SidebarComponent } from './main/components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    SharedModule.forRoot(),
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Sources/Pic.Client/src/app/shared/shared.module.ts
+++ Sources/Pic.Client/src/app/shared/shared.module.ts
@@ -1,20 +1,26 @@
 import { CommonModule } from "@angular/common";
 import { ModuleWithProviders, NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { RouterModule } from "@angular/router";
+import { MaterialDesignModule } from "./material-design.module";
 
 @NgModule({
 	imports: [
 		CommonModule,
 		ReactiveFormsModule,
 		FormsModule,
-		RouterModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    MaterialDesignModule,
 	],
 	exports: [
 		CommonModule,
 		ReactiveFormsModule,
 		FormsModule,
-		RouterModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    MaterialDesignModule,
 	],
 	declarations: [],
 	providers: [],
